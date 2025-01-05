e2fbfe72b4f1c3d3e18cc7764912e407683d2757
neo <gtugushi67@gmail.com>
2025-01-05 14:11:13 +0400

    add reference module break assemble  zshared assemble

---
+++ CSharp/src/Break.Module.Core/Astractions/Iservices/IBrakeTimeService.cs
@@ -0,0 +1,7 @@
+namespace Break.Module.Core.Astractions.Iservices
+{
+    public interface IBrakeTimeService
+    {
+
+    }
+}
--- CSharp/src/Break.Module.Core/BreakWorker/WorkerServiceHenlde.cs
+++ CSharp/src/Break.Module.Core/BreakWorker/WorkerServiceHenlde.cs
@@ -1,12 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Break.Module.Core.Astractions.Iservices;
 using Microsoft.Extensions.Logging;
 namespace Break.Module.Core.BreakWorker
 {
     public class WorkerServiceHenlde
     {
+        private readonly IBrakeTimeService BrakeService;
         private readonly ILogger<WorkerServiceHenlde> logger;
-        public WorkerServiceHenlde( ILogger<WorkerServiceHenlde> logger)
+        private readonly IPingSender _pingIpChecker;
+        public WorkerServiceHenlde( ILogger<WorkerServiceHenlde> logger 
+        ,IBrakeTimeService BrakeService)
+        => (this.BrakeService, this.logger) = (BrakeService, logger);
+          
+        
+       
+
+         public async Task AsyncMethodBreake()
         {
-            this.logger = logger;
+
+            try
+            { 
+                 // get user  -->>>>   from db
+
+                 var PingResponseStatus= await _pingIpChecker.PingIp("");
+
+                await BrakeService.addService(1, PingResponseStatus);
+            }
+
+            catch (Exception ex)
+            {
+
+
+                logger.LogError(ex, "Error in WorkerServiceHenlde");
+
+            }
         }
 
     }
