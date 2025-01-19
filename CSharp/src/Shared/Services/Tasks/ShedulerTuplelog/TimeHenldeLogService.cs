17dd03b27a7269395e8db119c2858316c548e4a0
neo <gtugushi67@gmail.com>
2025-01-19 21:24:59 +0400

    in  GetTimeResult add   case ResponseType.BrakeTime:

--- CSharp/src/Shared/Services/Tasks/ShedulerTuplelog/TimeHenldeLogService.cs
+++ CSharp/src/Shared/Services/Tasks/ShedulerTuplelog/TimeHenldeLogService.cs
@@ -10,6 +10,32 @@ namespace Shared.Services.Tasks.ShedulerTuplelog;
 
         public Task<object> GetTimeResult(TimeDtoReqvest entity, bool status, bool busy, ServiceResponseType responseType)
         {
-            throw new System.NotImplementedException();
+           if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            switch (responseType)
+            {
+                case ResponseType.BrakeTime:
+                    return await Task.FromResult(new ResponseResultBrakeTime
+                    {
+
+                        StartTimeValidWorkSchedule = entity != null && entity.StartTime != null &&
+                        entity.StartTime.Any() && status && busy == true,
+
+                        EndTimeLastMinute = entity?.EndTime != null && entity.EndTime.Any() && entity.EndTime.Last().Minute == DateTime.Now.Minute,
+                        StartTimeTimeLastMinute = entity?.StartTime != null && entity.StartTime.Any() && entity.StartTime.Last().Minute == DateTime.Now.Minute,
+                        OnlineTimeDateDay = (entity?.OnlineTime?.Any(day => day.Day == DateTime.Now.Day) ?? false) && !status,
+                        //  OnlineTimeDateDay = (entity?.OnlineTime?.Any() ?? false) && !status,??????
+                        StartTimeBreak = entity?.StartTime?.Any(day => day.Day == DateTime.Now.Day) == false,
+                        OfflineTimeDateDay = entity?.OflineTime?.Any(day => day.Day == DateTime.Now.Day) ?? false,
+                        workSchedulPingLog = !status && entity?.StartTime?.Any()==true &&
+                        entity?.OflineTime?.Any(day => day.Day == DateTime.Now.Day) == false && !busy
+                    });
+                case ResponseType.ComingAndgoing:
+                  
+
+                default:
+                    throw new ArgumentException("Invalid response type");
+            }
         }
     }
+        
