2000dfcd877c2f1218c9e9feccad9152c8813438
neo <gtugushi67@gmail.com>
2025-01-20 09:51:21 +0400

    modifed ract

--- NodeJs/src/user_core/src/getdata/dto/update-getdatum.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGetdatumDto } from './create-getdatum.dto';
-
-export class UpdateGetdatumDto extends PartialType(CreateGetdatumDto) {}
