7cfb7242bfe8445fcd958a873b477a80a9752abd
devg93 <gtugushi67@gmail.com>
2025-03-11 19:59:56 +0400

    start refactoring TimeInTimeOut.Module.Core

--- TimeControl_Core/src/Modules/TimeInTimeOut.Module.Core/TimeInTimeOutWorker/Mediator/ITimeInTimeOutMediator.cs
+++
@@ -1,8 +0,0 @@
-
-
-namespace TimeInTimeOut.Module.Core.TimeInTimeOutWorker.BackgroundService;
-
-    public interface ITimeInTimeOutMediator
-    {
-         Task<bool> UpdateTimeInTimeOutAsync(int userId, bool pingResponseStatus);
-    }
